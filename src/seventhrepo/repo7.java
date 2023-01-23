9f3e4477e5eca140547fce871eb0e867b589919c
Md Mujahid ul haq <Md Mujahid ul haq@DESKTOP-ODT7B24>
2023-01-23 10:25:25 -0600

    Poll scm

--- src/seventhrepo/repo7.java
+++ src/seventhrepo/repo7.java
@@ -5,6 +5,7 @@ public class repo7 {
 	public static void main(String[] args) {
 		System.out.println("My ninth repository");
 		System.out.println("multiplication");
+		System.out.println("Poll scm is the part of jenkins");
 
 	}
 
