931dbd0de1f950e4a84eb848fed962cf591dccbe
Md Mujahid ul haq <Md Mujahid ul haq@DESKTOP-ODT7B24>
2023-01-23 10:34:07 -0600

    subtraction

--- src/seventhrepo/repo7.java
+++ src/seventhrepo/repo7.java
@@ -6,6 +6,7 @@ public class repo7 {
 		System.out.println("My ninth repository");
 		System.out.println("multiplication");
 		System.out.println("Poll scm is the part of jenkins");
+		System.out.println(86-68);
 
 	}
 
